159bf29a26c5e1b7f0cc9a9d3a07b41548cc6103
Connor Munro <connor@pixelunion.net>
2015-05-20 10:12:54 -0400

    Add initial configuration (from Stencil)

---
+++ assets/config.js
@@ -0,0 +1,46 @@
+System.config({
+  "baseURL": "/assets/",
+  "transpiler": "babel",
+  "babelOptions": {
+    "optional": [
+      "runtime"
+    ]
+  },
+  "paths": {
+    "*": "*.js",
+    "github:*": "jspm_packages/github/*.js",
+    "npm:*": "jspm_packages/npm/*.js"
+  }
+});
+
+System.config({
+  "map": {
+    "asyncly/EventEmitter2": "github:asyncly/EventEmitter2@0.4.14",
+    "babel": "npm:babel@4.7.16",
+    "babel-runtime": "npm:babel-runtime@4.7.16",
+    "bigcommerce/stencil-utils": "github:bigcommerce/stencil-utils@0.0.6",
+    "caolan/async": "github:caolan/async@0.9.2",
+    "core-js": "npm:core-js@0.8.4",
+    "jquery": "github:components/jquery@2.1.4",
+    "knockout": "github:knockout/knockout@3.3.0",
+    "lodash": "npm:lodash@3.9.1",
+    "github:bigcommerce/stencil-utils@0.0.6": {
+      "asyncly/EventEmitter2": "github:asyncly/EventEmitter2@0.4.14",
+      "jquery": "github:components/jquery@2.1.4",
+      "lodash": "npm:lodash@3.9.1"
+    },
+    "github:jspm/nodelibs-process@0.1.1": {
+      "process": "npm:process@0.10.1"
+    },
+    "npm:babel-runtime@4.7.16": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:core-js@0.8.4": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:lodash@3.9.1": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    }
+  }
+});
+
