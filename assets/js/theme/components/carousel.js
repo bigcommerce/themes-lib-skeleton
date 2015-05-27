48e9db1b311d69871a0802e1d616375ac6e8eca3
Connor Munro <connor@pixelunion.net>
2015-05-27 16:23:07 -0400

    Fix formatting issues in carousel.js

--- assets/js/theme/components/carousel.js
+++ assets/js/theme/components/carousel.js
@@ -1,6 +1,6 @@
-import $ from "jquery";
-import imagesLoaded from "imagesloaded";
-import trend from "../plugins/trend";
+import $ from 'jquery';
+import imagesLoaded from 'imagesloaded';
+import trend from '../plugins/trend';
 
 export default class Carousel {
 
@@ -12,7 +12,7 @@ export default class Carousel {
             carouselDelay: 4000
         }, options);
 
-        this.carouselItem = this.$el.find(".carousel-item");
+        this.carouselItem = this.$el.find('.carousel-item');
 
         this.bindEvents();
         this.setCarouselHeight();
@@ -20,36 +20,35 @@ export default class Carousel {
     }
 
     bindEvents() {
-        this.$el.on("click", this.$el.find(".carousel-navigation-item"), (e) => {
-            if ($(e.target).hasClass("next")) {
+        this.$el.on('click', this.$el.find('.carousel-navigation-item'), (e) => {
+            if ($(e.target).hasClass('next')) {
                 this.nextSlide();
             } else {
                 this.previousSlide();
             }
         });
 
-        this.$el.on("mouseenter", () => {
+        this.$el.on('mouseenter', () => {
             this.pauseLoop();
         });
 
-        this.$el.on("mouseleave", () => {
+        this.$el.on('mouseleave', () => {
             this.startLoop();
         });
 
-        $(window).on("resize", () => {
+        $(window).on('resize', () => {
             this.setCarouselHeight();
         });
     }
 
     setCarouselHeight() {
-
         this.$el.imagesLoaded(() => {
             let carouselHeight = 0;
 
             for (let i of this.carouselItem.length) {
                 let carouselItemHeight = this.carouselItem.eq(i).height();
 
-                carouselHeight = Math.max(carouselHeight, carouselItemHeight)
+                carouselHeight = Math.max(carouselHeight, carouselItemHeight);
             }
 
             this.$el.height(carouselHeight);
@@ -57,20 +56,19 @@ export default class Carousel {
     }
 
     previousSlide() {
-        this.showNewSlide("prev");
+        this.showNewSlide('prev');
     }
 
     nextSlide() {
-        this.showNewSlide("next");
+        this.showNewSlide('next');
     }
 
     showNewSlide(type) {
+        let direction = (type === 'next') ? 'left' : 'right';
+        let fallback = (type === 'next') ? 'first' : 'last';
 
-        let direction = (type === "next") ? "left" : "right";
-        let fallback = (type === "next") ? "first" : "last"
-
-        let activeSlide = $(".carousel-item.active");
-        let nextSlide = (activeSlide[type]().hasClass("carousel-item")) ? activeSlide[type]() : this.carouselItem[fallback]();
+        let activeSlide = $('.carousel-item.active');
+        let nextSlide = (activeSlide[type]().hasClass('carousel-item')) ? activeSlide[type]() : this.carouselItem[fallback]();
 
         if (!nextSlide.length) {
             nextSlide = this.carouselItem[fallback]();
@@ -81,14 +79,13 @@ export default class Carousel {
         activeSlide.addClass(direction);
         nextSlide.addClass(direction);
 
-        nextSlide.one("trend", function() {
-            nextSlide.removeClass([type, direction].join(" ")).addClass("active");
-            activeSlide.removeClass(["active", direction].join(" "));
+        nextSlide.one('trend', function() {
+            nextSlide.removeClass([type, direction].join(' ')).addClass('active');
+            activeSlide.removeClass(['active', direction].join(' '));
         });
     }
 
     startLoop() {
-
         let delay = this.options.carouselDelay * 1000;
 
         this.autoplay = setInterval(() => {
