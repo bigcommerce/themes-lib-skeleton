aac627cfc63cad37e296bdafe39d8d7c8f318a74
Connor Munro <connor@pixelunion.net>
2015-08-31 15:52:42 -0400

    Fix indentation on global.js

--- assets/js/theme/global.js
+++ assets/js/theme/global.js
@@ -5,18 +5,18 @@ import quickSearch from './components/quick-search';
 import currencySelector from './components/currency-selector';
 
 export default class Global extends PageManager {
-    constructor() {
-        super();
-    }
+  constructor() {
+    super();
+  }
 
-    /**
-     * You can wrap the execution in this method with an asynchronous function map using the async library
-     * if your components modules need async callback handling.
-     * @param next
-     */
-    loaded(next) {
-        quickSearch();
-        currencySelector();
-        next();
-    }
+  /**
+   * You can wrap the execution in this method with an asynchronous function map using the async library
+   * if your components modules need async callback handling.
+   * @param next
+   */
+  loaded(next) {
+    quickSearch();
+    currencySelector();
+    next();
+  }
 }
