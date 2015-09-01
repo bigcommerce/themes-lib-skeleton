3869e7da50b110b96358219119588b9a7963dba9
Connor Munro <connor@pixelunion.net>
2015-09-01 13:05:28 -0400

    Remove cart js

--- assets/js/theme/cart.js
+++ assets/js/theme/cart.js
@@ -1,52 +1,9 @@
-import PageManager from '../page-manager';
 import $ from 'jquery';
+import PageManager from '../page-manager';
 import utils from 'bigcommerce/stencil-utils';
 
 export default class Cart extends PageManager {
-    loaded(next) {
-
-        // cart update
-        $('body').on('click', '.cart-update', (event) => {
-            let $target = $(event.currentTarget),
-                itemId = $target.data('cart-itemid'),
-                el = $('#qty-' + itemId),
-                oldQty = parseInt(el.text()),
-                newQty;
-
-            event.preventDefault();
-
-            newQty = $target.data('action') === 'inc' ? oldQty + 1 : oldQty - 1;
-            el.text(newQty);
-            utils.cart.itemUpdate(itemId, newQty, (err, response) => {
-                if (response.status === 'succeed') {
-                    this.refreshContent();
-                } else {
-                    el.text(oldQty);
-                    alert(response.errors.join('\n'));
-                }
-            });
-        });
-
-        $('body').on('click', '.cart-remove', (event) => {
-            let itemId = $(event.currentTarget).data('cart-itemid');
-
-            event.preventDefault();
-
-            utils.cart.itemRemove(itemId, (err, response) => {
-                if (response.status === 'succeed') {
-                    this.refreshContent();
-                } else {
-                    alert(response.errors.join('\n'));
-                }
-            });
-        });
-
-        next();
-    }
-
-    refreshContent() {
-        utils.cart.getContent({render_with: 'cart/content'}, (err, content) => {
-            $('[data-cart-content]').html(content);
-        });
-    }
+  constructor() {
+    super();
+  }
 }
