90fe5e7b4966e3de4e21d90bd7dd3823f9248540
Connor Munro <connor@pixelunion.net>
2015-09-10 13:43:31 -0400

    Add faceted search

--- assets/config.js
+++ assets/config.js
@@ -24,12 +24,14 @@ System.config({
     "bigcommerce/stencil-utils": "github:bigcommerce/stencil-utils@0.3.2",
     "caolan/async": "github:caolan/async@0.9.2",
     "core-js": "npm:core-js@0.8.4",
+    "history": "github:browserstate/history.js@1.8.0",
     "imagesloaded": "npm:imagesloaded@3.1.8",
     "jquery": "github:components/jquery@2.1.4",
     "jquery-trend": "npm:jquery-trend@0.1.0",
     "knockout": "github:knockout/knockout@3.3.0",
     "lodash": "npm:lodash@3.9.3",
     "normalize.scss": "npm:normalize.scss@0.1.0",
+    "url": "github:jspm/nodelibs-url@0.1.0",
     "bitbucket:pixelunion/bc-carousel@2.1.0": {
       "imagesloaded": "npm:imagesloaded@3.1.8",
       "jquery": "github:components/jquery@2.1.4",
@@ -48,8 +50,20 @@ System.config({
       "jquery": "github:components/jquery@2.1.4",
       "lodash": "npm:lodash@3.9.3"
     },
+    "github:jspm/nodelibs-assert@0.1.0": {
+      "assert": "npm:assert@1.3.0"
+    },
     "github:jspm/nodelibs-process@0.1.1": {
       "process": "npm:process@0.10.1"
+    },
+    "github:jspm/nodelibs-url@0.1.0": {
+      "url": "npm:url@0.10.3"
+    },
+    "github:jspm/nodelibs-util@0.1.0": {
+      "util": "npm:util@0.10.3"
+    },
+    "npm:assert@1.3.0": {
+      "util": "npm:util@0.10.3"
     },
     "npm:babel-runtime@4.7.16": {
       "process": "github:jspm/nodelibs-process@0.1.1"
@@ -61,7 +75,23 @@ System.config({
       "eventie": "npm:eventie@1.0.6",
       "wolfy87-eventemitter": "npm:wolfy87-eventemitter@4.2.11"
     },
+    "npm:inherits@2.0.1": {
+      "util": "github:jspm/nodelibs-util@0.1.0"
+    },
     "npm:lodash@3.9.3": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:punycode@1.3.2": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:url@0.10.3": {
+      "assert": "github:jspm/nodelibs-assert@0.1.0",
+      "punycode": "npm:punycode@1.3.2",
+      "querystring": "npm:querystring@0.2.0",
+      "util": "github:jspm/nodelibs-util@0.1.0"
+    },
+    "npm:util@0.10.3": {
+      "inherits": "npm:inherits@2.0.1",
       "process": "github:jspm/nodelibs-process@0.1.1"
     },
     "npm:wolfy87-eventemitter@4.2.11": {
--- assets/js/theme/category.js
+++ assets/js/theme/category.js
@@ -1,7 +1,39 @@
 import PageManager from '../page-manager';
+import FacetedSearch from './components/faceted-search';
+import Loading from 'bc-loading';
 
 export default class Category extends PageManager {
   constructor() {
     super();
+
+    const loadingOptions = {
+      loadingMarkup: '<div class="loading"><span class="loading-spinner"></span></div>',
+      visibleClass: 'visible',
+      scrollLockClass: 'scroll-locked',
+    };
+
+    if ($('[data-faceted-search]').length) {
+      this._initializeFacetedSearch();
+      this.sidebarOverlay = new Loading(loadingOptions, true, '[data-category-sidebar]');
+    }
+  }
+
+  _initializeFacetedSearch() {
+    const facetedSearchOptions = {
+      config: {
+        category: {
+          shop_by_price: true,
+        },
+      },
+      template: {
+        productListing: 'category/product-listing',
+        sidebar: 'category/sidebar',
+      },
+    };
+
+    const facetedSearch = new FacetedSearch(facetedSearchOptions, {
+      willUpdate: () => this.sidebarOverlay.show(),
+      didUpdate: () => this.sidebarOverlay.hide(),
+    });
   }
 }
