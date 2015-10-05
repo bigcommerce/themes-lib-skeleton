0b73ebedef36d121c2f52467956cb015987e5119
Connor Munro <connor@pixelunion.net>
2015-10-05 15:03:56 -0400

    Add select wrapper class

--- assets/js/theme/global.js
+++ assets/js/theme/global.js
@@ -1,11 +1,19 @@
 import $ from 'jquery';
 import PageManager from '../page-manager';
 import CurrencySelector from './components/currency-selector';
+import SelectWrapper from './components/select-wrapper';
 
 export default class Global extends PageManager {
   constructor() {
     super();
 
     new CurrencySelector('[data-currency-switcher]');
+
+    const $select = $('select');
+    if ($select.length) {
+      $select.each((i, el) => {
+        new SelectWrapper(el);
+      });
+    }
   }
 }
