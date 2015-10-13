41fba5718ee4e505c7a4074e99b6a9629ac32b30
Connor Munro <connor@pixelunion.net>
2015-10-13 10:20:07 -0400

    Add brands markup/facted search initialization

--- assets/js/theme/Brand.js
+++ assets/js/theme/Brand.js
@@ -1,7 +1,32 @@
 import PageManager from '../PageManager';
+import FacetedSearch from './components/FacetedSearch';
+import Loading from 'bc-loading';
 
 export default class Brand extends PageManager {
   constructor() {
     super();
+
+    if ($('[data-faceted-search]').length) {
+      this._initializeFacetedSearch();
+    }
+  }
+
+  _initializeFacetedSearch() {
+    const facetedSearchOptions = {
+      template: {
+        productListing: 'brand/product-listing',
+        sidebar: 'brand/sidebar',
+      },
+      scope: {
+        productListing: '[data-brand]',
+        sidebar: '[data-brand-sidebar]',
+      },
+      // callbacks: {
+      //   willUpdate: () => {},
+      //   didUpdate: () => {},
+      // }
+    };
+
+    new FacetedSearch(facetedSearchOptions);
   }
 }
