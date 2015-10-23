48cb181015aa4ee84bafda586b3e311deba55a9c
essmahr <essmahr@gmail.com>
2015-10-23 13:42:56 -0700

    update selectWrapper to assume wrapper div by default

--- assets/js/theme/components/SelectWrapper.js
+++ assets/js/theme/components/SelectWrapper.js
@@ -1,18 +1,25 @@
 import $ from 'jquery';
 
+/*
+ * Prepends a span containing the currently selected text value to matching select elements
+ * and updates that span's text on input change.
+ *
+ * Requires for the <select> to have a direct parent .form-select-wrapper element.
+ */
 export default class SelectWrapper {
   constructor(el) {
     this.$el = $(el);
+    this.$parent = this.$el.parent('.form-select-wrapper');
 
-    if (!this.$el.parent('.form-select-wrapper').length) {
-      this.$el.wrap('<div class="form-select-wrapper" />').parent().prepend(`<span class="selected-text">${this.$el.find('option:selected').text()}</span>`);
+    // only run if wrapper is in place AND it's not the currency selector
+    if (this.$parent.length && !this.$parent.closest('[data-currency-selector]').length) {
+      this._init();
     }
+  }
 
-    // Conditional required so I can use the currency code as opposed to
-    // the full currency name in the currency switcher
-    if (!this.$el.closest('.currency-switcher').length) {
-      this._bindEvents();
-    }
+  _init() {
+    this.$el.before(`<span class="form-selected-text">${this.$el.find('option:selected').text()}</span>`);
+    this._bindEvents();
   }
 
   _bindEvents() {
@@ -23,6 +30,6 @@ export default class SelectWrapper {
 
   updateSelectText(option) {
     const newOption = option ? option : this.$el.find('option:selected').text();
-    this.$el.siblings('.selected-text').text(newOption);
+    this.$el.siblings('.form-selected-text').text(newOption);
   }
 }
